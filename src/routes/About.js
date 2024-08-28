907b80ede3194e188b9e8a94d9c29926e4dc479d
Luis Baptista <38134147+lbaptista95@users.noreply.github.com>
2024-08-28 19:11:16 -0300

    About background adjustment

--- src/routes/About.js
+++ src/routes/About.js
@@ -10,29 +10,31 @@ export default function About() {
 
     const { name, pictureUrl, email, phone, bio, links } = aboutInfo
 
-    const linkButtons = <div className="about-main-link-buttons">{links.map(link => <LinkButton link={link}/>)}</div> 
+    const linkButtons = <div className="about-main-link-buttons">{links.map(link => <LinkButton link={link} />)}</div>
 
     return (
-        <div className="about">
+        <div>
             <Header title={headers[2].title} subtitle={headers[2].subtitle} />
-            <div className="about-main">
-                <div className="about-main-top">
-                    <div className="about-main-picture"></div>
-                    <div className="about-main-info">
-                        <h3 className="about-main-info-name">{name}</h3>
-                        <div className="about-main-info-other">
-                            <img src="https://static.vecteezy.com/system/resources/previews/031/400/781/original/email-icon-png-free-vector.jpg" className="about-main-info-other-icon" />
-                            <h4>{email}</h4>
+            <div className="about">
+                <div className="about-main">
+                    <div className="about-main-top">
+                        <div className="about-main-picture"></div>
+                        <div className="about-main-info">
+                            <h3 className="about-main-info-name">{name}</h3>
+                            <div className="about-main-info-other">
+                                <img src="https://static.vecteezy.com/system/resources/previews/031/400/781/original/email-icon-png-free-vector.jpg" className="about-main-info-other-icon" />
+                                <h4>{email}</h4>
+                            </div>
+                            <div className="about-main-info-other">
+                                <img src="https://icons.veryicon.com/png/o/object/material-design-icons/phone-80.png" className="about-main-info-other-icon" />
+                                <h4>{phone}</h4>
+                            </div>
+                            {linkButtons}
                         </div>
-                        <div className="about-main-info-other">
-                            <img src="https://icons.veryicon.com/png/o/object/material-design-icons/phone-80.png" className="about-main-info-other-icon" />
-                            <h4>{phone}</h4>
-                        </div>
-                        {linkButtons}
                     </div>
-                </div>
-                <div className="about-main-bottom">
-                    <p className="about-main-bottom-text">{bio}</p>
+                    <div className="about-main-bottom">
+                        <p className="about-main-bottom-text">{bio}</p>
+                    </div>
                 </div>
             </div>
         </div>
